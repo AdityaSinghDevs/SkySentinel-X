e3c636dc336095b615321df5657d188beb2dd2ad
AdityaSinghDevs <adityasingh.devs@gmail.com>
2025-09-05 23:36:44 +0530

    traininng logger added

---
+++ utils/logger_test.py
@@ -0,0 +1,5 @@
+from train_logger import get_train_logger
+
+logger = get_train_logger(__name__)
+
+logger.info("hello")
--- utils/train_logger.py
+++ utils/train_logger.py
@@ -2,7 +2,8 @@ import logging
 import os
 from typing import Optional
 
-def train_logger(name:str, log_level: str = 'INFO',
+def get_train_logger(name:str, log_level: str = 'INFO',
+                 console : bool = True,
                  )->logging.Logger:
 
     logger = logging.getLogger(name)
@@ -13,3 +14,22 @@ def train_logger(name:str, log_level: str = 'INFO',
 
         if console:
             console_handler = logging.StreamHandler()
+        console_format = logging.Formatter("{name}- {levelname} - {message}" , style='{')
+
+        console_handler.setFormatter(console_format)
+        logger.addHandler(console_handler)
+
+        base_log_dir = "../logs"
+
+        log_file = os.path.join(base_log_dir, "train.log")
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+        file_handler = logging.FileHandler(log_file, mode="a" , encoding= 'utf-8')
+
+        file_format = logging.Formatter("{asctime}-{levelname}-{name}:{funcName}:L{lineno}:{message}" , style="{")
+
+        file_handler.setFormatter(file_format)
+        logger.addHandler(file_handler)
+        
+    return logger
+            
